e45f59949c7e2b8f19cb5c1f6dc35e121a2c2a24
Egoitz <eladron002@ikasle.ehu.eus>
2025-10-21 11:50:49 +0200

    Simple Factory

--- src/domain/Symptom.java
+++ src/domain/Symptom.java
@@ -1,6 +1,6 @@
 package domain;
 
-public class Symptom {
+public abstract class Symptom {
 	private String name;
 	private int covidImpact;
 	private int severityIndex;
--- src/factory/Main.java
+++ src/factory/Main.java
@@ -6,9 +6,10 @@ import domain.Medicament;
 public class Main {
 
 	public static void main(String[] args) {
-		Covid19Pacient p1=new Covid19Pacient("aitor", 35);
+		SymptomFactory sF = new SymptomFactory();
+		Covid19Pacient p1=new Covid19Pacient("aitor", 35, sF);
 		new PacientSymptomGUI(p1);
-		new MedicalGUI(new Medicament("Ibuprofeno"));
+		new MedicalGUI(new Medicament("Ibuprofeno", sF));
 
 	}
 
