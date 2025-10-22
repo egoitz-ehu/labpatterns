ce205c2a6e9619e44c116668962abc98a57ac1ff
Egoitz <eladron002@ikasle.ehu.eus>
2025-10-22 11:28:34 +0200

    Observer patroia 2.prototipoa

--- src/observer/Main.java
+++ src/observer/Main.java
@@ -13,9 +13,15 @@ public class Main {
 		Observable	pacient=new	Covid19Pacient("aitor",	35);
 		new	PacientObserverGUI(pacient);
 		new	PacientSymptomGUI((Covid19Pacient) pacient);
-		Observable pacient2 = new Covid19Pacient("Aiur",24);
-		new PacientObserverGUI(pacient2);
-		new PacientSymptomGUI((Covid19Pacient) pacient2);
+		new PacientThermometerGUI(pacient);
+		Observable	pacient2=new	Covid19Pacient("aitor2",	35);
+		new	PacientObserverGUI(pacient2);
+		new	PacientSymptomGUI((Covid19Pacient) pacient2);
+		new PacientThermometerGUI(pacient2);
+		Observable	pacient3=new	Covid19Pacient("aitor3",	35);
+		new	PacientObserverGUI(pacient3);
+		new	PacientSymptomGUI((Covid19Pacient) pacient3);
+		new PacientThermometerGUI(pacient3);
 	}
 
 
