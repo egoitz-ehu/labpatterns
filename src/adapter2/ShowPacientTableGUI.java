70357dd3c5987cb4ce13eae0954116c1ba7f9516
Egoitz <eladron002@ikasle.ehu.eus>
2025-10-24 17:25:14 +0200

    Adapter ariketa hautazkoa

--- src/adapter2/ShowPacientTableGUI.java
+++ src/adapter2/ShowPacientTableGUI.java
@@ -2,6 +2,8 @@ package adapter2;
 
 import java.awt.Component;
 import java.awt.Font;
+import java.util.Observable;
+import java.util.Observer;
 
 import javax.swing.*;
 import javax.swing.table.TableModel;
@@ -9,20 +11,21 @@ import javax.swing.table.TableModel;
 import domain.Covid19Pacient;
 
 
-public class ShowPacientTableGUI extends JFrame{
+public class ShowPacientTableGUI extends JFrame implements Observer{
 	
 	JTable table;
-	Covid19Pacient pacient;
+	private Observable o;
+	private TableModel tm;
 	
   
-  public ShowPacientTableGUI(Covid19Pacient pacient ) {
-	  	this.setTitle("Covid Symptoms "+pacient.getName());
+  public ShowPacientTableGUI(Observable o) {
+	  	this.setTitle("Covid Symptoms "+((Covid19Pacient)o).getName());
 	  	
-	  	this.pacient=pacient;
+	  	this.o=o;
 	  	
 	  	setFonts();
 	    
-	  	TableModel tm=new Covid19PacientTableModelAdapter(pacient);
+	  	tm=new Covid19PacientTableModelAdapter((Covid19Pacient)o);
 		table = new JTable(tm);
 	    table.setRowHeight(36);
 	    JScrollPane pane = new JScrollPane(table);
@@ -30,12 +33,18 @@ public class ShowPacientTableGUI extends JFrame{
 	      new java.awt.Dimension(300, 200));
 	    this.getContentPane().add(pane);
 	    
-	  
+	  o.addObserver(this);
   }
 
-  private static void setFonts() {
-    Font font = new Font("Dialog", Font.PLAIN, 18);
-    UIManager.put("Table.font", font);
-    UIManager.put("TableHeader.font", font);
-  }
+	  private static void setFonts() {
+	    Font font = new Font("Dialog", Font.PLAIN, 18);
+	    UIManager.put("Table.font", font);
+	    UIManager.put("TableHeader.font", font);
+	  }
+
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		tm = new Covid19PacientTableModelAdapter((Covid19Pacient)o);
+		table.setModel(tm);
+	}
 }
--- src/observer/Main.java
+++ src/observer/Main.java
@@ -2,6 +2,7 @@ package observer;
 
 import java.util.Observable;
 
+import adapter2.ShowPacientTableGUI;
 import domain.Covid19Pacient;
 
 public class Main {
@@ -15,14 +16,28 @@ public class Main {
 		new	PacientSymptomGUI((Covid19Pacient) pacient);
 		new PacientThermometerGUI(pacient);
 		new SemaphorGUI(pacient);
+		ShowPacientTableGUI gui1=new ShowPacientTableGUI(pacient);
+		gui1.setPreferredSize(
+			      new java.awt.Dimension(300, 200));
+		gui1.setVisible(true);
 		Observable	pacient2=new	Covid19Pacient("aitor2",	35);
 		new	PacientObserverGUI(pacient2);
 		new	PacientSymptomGUI((Covid19Pacient) pacient2);
 		new PacientThermometerGUI(pacient2);
+		new SemaphorGUI(pacient2);
+		ShowPacientTableGUI gui2=new ShowPacientTableGUI(pacient2);
+		gui2.setPreferredSize(
+			      new java.awt.Dimension(300, 200));
+		gui2.setVisible(true);
 		Observable	pacient3=new	Covid19Pacient("aitor3",	35);
 		new	PacientObserverGUI(pacient3);
 		new	PacientSymptomGUI((Covid19Pacient) pacient3);
 		new PacientThermometerGUI(pacient3);
+		new SemaphorGUI(pacient3);
+		ShowPacientTableGUI gui3=new ShowPacientTableGUI(pacient3);
+		gui3.setPreferredSize(
+			      new java.awt.Dimension(300, 200));
+		gui3.setVisible(true);
 	}
 
 
